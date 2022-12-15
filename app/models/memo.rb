f168f868acdfe932dd5fea77fa4bb2346b714f67
h-takahashi-git <takahashishigoto@gmail.com>
2022-12-15 17:43:26 +0900

    これで引っかからないとビビる

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -3,4 +3,17 @@
 class Memo < ApplicationRecord
   validates :name, length: { maximum: 20 }, presence: true
   validates :comment, length: { maximum: 100 }, presence: true
+
+
+  def hoge
+    # MySQL に接続します。
+      my = Mysql.new('hogehoge.db.jjjjj121j.jp','admin','eawrewafd2','dbname')
+
+      # SQL クエリを実行します。
+      res = my.query('SELECT * from table_heteml')
+
+      # 結果を表示します。
+      res.eachdo|row|
+      puts row[0]
+  end
 end
